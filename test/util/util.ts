707b89ab8b8f93378218af16dbc3d456085b4779
dragolea <dragolea@yahoo.com>
2023-10-02 10:05:17 +0300

    Changes to test

--- test/util/util.ts
+++ test/util/util.ts
@@ -1,26 +1,26 @@
 import path from 'path';
 import cds from '@sap/cds';
 import BaseRepository from '../../lib/BaseRepository';
-import { Book } from '../bookshop/srv/util/types/entities/sap/capire/bookshop';
+import { type Book } from '../bookshop/srv/util/types/entities/sap/capire/bookshop';
 
-export const connectTest = (dirName : string, projectName : string) => {
-    const project = path.join(dirName, projectName);
-    cds.test(project)
+export const connectTest = (dirName: string, projectName: string) => {
+  const project = path.join(dirName, projectName);
+  cds.test(project);
 
-    return cds
-}
+  return cds;
+};
 
-export const getCustomerRepository = async (_cds : ReturnType<typeof connectTest>) => {
-    const CatalogService = await _cds.connect.to('CatalogService')
-    const { Books } = CatalogService.entities
+export const getBookRepository = async (_cds: ReturnType<typeof connectTest>) => {
+  const CatalogService = await _cds.connect.to('CatalogService');
+  const { Books } = CatalogService.entities;
 
-    class CustomerRepository extends BaseRepository<Book> {
-        constructor() {
-          super(Books);
-        }
-      }
-    
-    return new CustomerRepository();
+  class BookRepository extends BaseRepository<Book> {
+    constructor() {
+      super(Books);
+    }
+  }
 
-    // return customer;
-}
+  return new BookRepository();
+
+  // return customer;
+};
