d3de98caa396897a2a4d456c7564c4313ef5b9ca
dragolea <dragolea@yahoo.com>
2023-10-17 13:19:28 +0300

    Update UPDATE.test.ts
    Updated tests for updateLocaleTexts()

--- test/unit/UPDATE.test.ts
+++ test/unit/UPDATE.test.ts
@@ -15,39 +15,22 @@ describe('UPDATE', () => {
     });
   });
 
-  // TODO DELETE
-  // !!!!
-  // describe('.updateMany()', () => {
-  //   test('It should RETURN : updateMany() multiple items from the db', async () => {
-  //     const bookRepository = await getBookRepository(cds);
-
-  //     const findAnItem = await bookRepository.find({ ID: 271 });
-  //     const findTheOtherItem = await bookRepository.find({ ID: 252 });
-
-  //     const findAnItemAfterUpdate = await bookRepository.find({ ID: 271 });
-  //     const findTheOtherItemAfterUpdate = await bookRepository.find({ ID: 252 });
-
-  //     expect(updatedItem).toBe(true);
-  //     expect([findAnItem, findTheOtherItem]).not.toMatchObject([findAnItemAfterUpdate, findTheOtherItemAfterUpdate]);
-
-  //     // !!!! TODO arguments types are ugly ...
-  //   });
-  // });
-
   describe('.updateLocaleTexts()', () => {
     test('It should RETURN : updateLocalTexts() update "Locale" languages text', async () => {
       const bookRepository = await getBookRepository(cds);
+      const locale = 'de';
+      const textToUpdate = 'a NEW TITLE : UPDATED';
 
       const updatedLocaleItem = await bookRepository.updateLocaleTexts(
-        { locale: 'de', ID: 201 },
-        { title: 'a NEW TITLE : UPDATED' },
+        { locale: locale, ID: 201 },
+        { title: textToUpdate },
       );
 
-      expect(updatedLocaleItem).toBe(true);
-
       const allLocalizedTexts = await bookRepository.getLocaleTexts();
+      const foundUpdated = allLocalizedTexts.filter((item) => item.ID === 201 && item.locale === locale);
 
-      // TODO make the test better
+      expect(updatedLocaleItem).toBe(true);
+      expect(foundUpdated[0].title).toBe(textToUpdate);
     });
   });
 });
