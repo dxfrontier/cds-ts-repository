1d07f80987be40534067ad91d683cbed0f4e1c6c
dragolea <dragolea@yahoo.com>
2023-12-18 16:14:26 +0200

    Export all types from types.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,6 @@
 export * from './BaseRepository';
 export * from './BaseRepositoryDraft';
 export * from './util/Filter';
-
-export type {
-  KeyValueType,
-  KeyValueDraftType,
-  Locale,
-  InsertResult,
-  FilterOperator,
-  LogicalOperator,
-  TypedRequest,
-} from './types/types';
+export type * from './types/types';
 
 export { Mixin } from 'ts-mixer';
