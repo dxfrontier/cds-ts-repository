dffaa90be852b3566871b738ac74d5f76cdd0346
dragolea <dragolea@yahoo.com>
2024-10-29 18:55:28 +0200

    chore(tests): added the cds-modesl of the API_BUSINESS_PARTNER

---
+++ test/bookshop/@cds-models/API_BUSINESS_PARTNER/index.js
@@ -0,0 +1,190 @@
+// This is an automatically generated file. Please do not change its contents manually!
+const cds = require('@sap/cds')
+const csn = cds.entities('API_BUSINESS_PARTNER')
+// service
+const API_BUSINESS_PARTNER = { name: 'API_BUSINESS_PARTNER' }
+module.exports = API_BUSINESS_PARTNER
+module.exports.API_BUSINESS_PARTNER = API_BUSINESS_PARTNER
+// A_AddressEmailAddress
+module.exports.A_AddressEmailAddress = { is_singular: true, __proto__: csn.A_AddressEmailAddress }
+module.exports.A_AddressEmailAddress_ = csn.A_AddressEmailAddress
+// A_AddressFaxNumber
+module.exports.A_AddressFaxNumber = { is_singular: true, __proto__: csn.A_AddressFaxNumber }
+module.exports.A_AddressFaxNumber_ = csn.A_AddressFaxNumber
+// A_AddressHomePageURL
+module.exports.A_AddressHomePageURL = { is_singular: true, __proto__: csn.A_AddressHomePageURL }
+module.exports.A_AddressHomePageURL_ = csn.A_AddressHomePageURL
+// A_AddressPhoneNumber
+module.exports.A_AddressPhoneNumber = { is_singular: true, __proto__: csn.A_AddressPhoneNumber }
+module.exports.A_AddressPhoneNumber_ = csn.A_AddressPhoneNumber
+// A_BPAddrDepdntIntlLocNumber
+module.exports.A_BPAddrDepdntIntlLocNumber = { is_singular: true, __proto__: csn.A_BPAddrDepdntIntlLocNumber }
+module.exports.A_BPAddrDepdntIntlLocNumber_ = csn.A_BPAddrDepdntIntlLocNumber
+// A_BPAddressIndependentEmail
+module.exports.A_BPAddressIndependentEmail = { is_singular: true, __proto__: csn.A_BPAddressIndependentEmail }
+module.exports.A_BPAddressIndependentEmail_ = csn.A_BPAddressIndependentEmail
+// A_BPAddressIndependentFax
+module.exports.A_BPAddressIndependentFax = { is_singular: true, __proto__: csn.A_BPAddressIndependentFax }
+module.exports.A_BPAddressIndependentFax_ = csn.A_BPAddressIndependentFax
+// A_BPAddressIndependentMobile
+module.exports.A_BPAddressIndependentMobile = { is_singular: true, __proto__: csn.A_BPAddressIndependentMobile }
+module.exports.A_BPAddressIndependentMobile_ = csn.A_BPAddressIndependentMobile
+// A_BPAddressIndependentPhone
+module.exports.A_BPAddressIndependentPhone = { is_singular: true, __proto__: csn.A_BPAddressIndependentPhone }
+module.exports.A_BPAddressIndependentPhone_ = csn.A_BPAddressIndependentPhone
+// A_BPAddressIndependentWebsite
+module.exports.A_BPAddressIndependentWebsite = { is_singular: true, __proto__: csn.A_BPAddressIndependentWebsite }
+module.exports.A_BPAddressIndependentWebsite_ = csn.A_BPAddressIndependentWebsite
+// A_BPContactToAddress
+module.exports.A_BPContactToAddress = { is_singular: true, __proto__: csn.A_BPContactToAddress }
+module.exports.A_BPContactToAddress_ = csn.A_BPContactToAddress
+// A_BPContactToFuncAndDept
+module.exports.A_BPContactToFuncAndDept = { is_singular: true, __proto__: csn.A_BPContactToFuncAndDept }
+module.exports.A_BPContactToFuncAndDept_ = csn.A_BPContactToFuncAndDept
+// A_BPCreditWorthiness
+module.exports.A_BPCreditWorthiness = { is_singular: true, __proto__: csn.A_BPCreditWorthiness }
+module.exports.A_BPCreditWorthiness_ = csn.A_BPCreditWorthiness
+// A_BPDataController
+module.exports.A_BPDataController = { is_singular: true, __proto__: csn.A_BPDataController }
+module.exports.A_BPDataController_ = csn.A_BPDataController
+// A_BPEmployment
+module.exports.A_BPEmployment = { is_singular: true, __proto__: csn.A_BPEmployment }
+module.exports.A_BPEmployment_ = csn.A_BPEmployment
+// A_BPFinancialServicesExtn
+module.exports.A_BPFinancialServicesExtn = { is_singular: true, __proto__: csn.A_BPFinancialServicesExtn }
+module.exports.A_BPFinancialServicesExtn_ = csn.A_BPFinancialServicesExtn
+// A_BPFinancialServicesReporting
+module.exports.A_BPFinancialServicesReporting = { is_singular: true, __proto__: csn.A_BPFinancialServicesReporting }
+module.exports.A_BPFinancialServicesReporting_ = csn.A_BPFinancialServicesReporting
+// A_BPFiscalYearInformation
+module.exports.A_BPFiscalYearInformation = { is_singular: true, __proto__: csn.A_BPFiscalYearInformation }
+module.exports.A_BPFiscalYearInformation_ = csn.A_BPFiscalYearInformation
+// A_BPIntlAddressVersion
+module.exports.A_BPIntlAddressVersion = { is_singular: true, __proto__: csn.A_BPIntlAddressVersion }
+module.exports.A_BPIntlAddressVersion_ = csn.A_BPIntlAddressVersion
+// A_BPRelationship
+module.exports.A_BPRelationship = { is_singular: true, __proto__: csn.A_BPRelationship }
+module.exports.A_BPRelationship_ = csn.A_BPRelationship
+// A_BuPaAddressUsage
+module.exports.A_BuPaAddressUsage = { is_singular: true, __proto__: csn.A_BuPaAddressUsage }
+module.exports.A_BuPaAddressUsage_ = csn.A_BuPaAddressUsage
+// A_BuPaIdentification
+module.exports.A_BuPaIdentification = { is_singular: true, __proto__: csn.A_BuPaIdentification }
+module.exports.A_BuPaIdentification_ = csn.A_BuPaIdentification
+// A_BuPaIndustry
+module.exports.A_BuPaIndustry = { is_singular: true, __proto__: csn.A_BuPaIndustry }
+module.exports.A_BuPaIndustry_ = csn.A_BuPaIndustry
+// A_BusinessPartner
+module.exports.A_BusinessPartner = { is_singular: true, __proto__: csn.A_BusinessPartner }
+module.exports.A_BusinessPartner_ = csn.A_BusinessPartner
+// A_BusinessPartnerAddress
+module.exports.A_BusinessPartnerAddress = { is_singular: true, __proto__: csn.A_BusinessPartnerAddress }
+module.exports.A_BusinessPartnerAddress_ = csn.A_BusinessPartnerAddress
+// A_BusinessPartnerAlias
+module.exports.A_BusinessPartnerAlia = { is_singular: true, __proto__: csn.A_BusinessPartnerAlias }
+module.exports.A_BusinessPartnerAlias = csn.A_BusinessPartnerAlias
+// A_BusinessPartnerBank
+module.exports.A_BusinessPartnerBank = { is_singular: true, __proto__: csn.A_BusinessPartnerBank }
+module.exports.A_BusinessPartnerBank_ = csn.A_BusinessPartnerBank
+// A_BusinessPartnerContact
+module.exports.A_BusinessPartnerContact = { is_singular: true, __proto__: csn.A_BusinessPartnerContact }
+module.exports.A_BusinessPartnerContact_ = csn.A_BusinessPartnerContact
+// A_BusinessPartnerIsBank
+module.exports.A_BusinessPartnerIsBank = { is_singular: true, __proto__: csn.A_BusinessPartnerIsBank }
+module.exports.A_BusinessPartnerIsBank_ = csn.A_BusinessPartnerIsBank
+// A_BusinessPartnerPaymentCard
+module.exports.A_BusinessPartnerPaymentCard = { is_singular: true, __proto__: csn.A_BusinessPartnerPaymentCard }
+module.exports.A_BusinessPartnerPaymentCard_ = csn.A_BusinessPartnerPaymentCard
+// A_BusinessPartnerRating
+module.exports.A_BusinessPartnerRating = { is_singular: true, __proto__: csn.A_BusinessPartnerRating }
+module.exports.A_BusinessPartnerRating_ = csn.A_BusinessPartnerRating
+// A_BusinessPartnerRole
+module.exports.A_BusinessPartnerRole = { is_singular: true, __proto__: csn.A_BusinessPartnerRole }
+module.exports.A_BusinessPartnerRole_ = csn.A_BusinessPartnerRole
+// A_BusinessPartnerTaxNumber
+module.exports.A_BusinessPartnerTaxNumber = { is_singular: true, __proto__: csn.A_BusinessPartnerTaxNumber }
+module.exports.A_BusinessPartnerTaxNumber_ = csn.A_BusinessPartnerTaxNumber
+// A_BusPartAddrDepdntTaxNmbr
+module.exports.A_BusPartAddrDepdntTaxNmbr = { is_singular: true, __proto__: csn.A_BusPartAddrDepdntTaxNmbr }
+module.exports.A_BusPartAddrDepdntTaxNmbr_ = csn.A_BusPartAddrDepdntTaxNmbr
+// A_CustAddrDepdntExtIdentifier
+module.exports.A_CustAddrDepdntExtIdentifier = { is_singular: true, __proto__: csn.A_CustAddrDepdntExtIdentifier }
+module.exports.A_CustAddrDepdntExtIdentifier_ = csn.A_CustAddrDepdntExtIdentifier
+// A_CustAddrDepdntInformation
+module.exports.A_CustAddrDepdntInformation = { is_singular: true, __proto__: csn.A_CustAddrDepdntInformation }
+module.exports.A_CustAddrDepdntInformation_ = csn.A_CustAddrDepdntInformation
+// A_Customer
+module.exports.A_Customer = { is_singular: true, __proto__: csn.A_Customer }
+module.exports.A_Customer_ = csn.A_Customer
+// A_CustomerCompany
+module.exports.A_CustomerCompany = { is_singular: true, __proto__: csn.A_CustomerCompany }
+module.exports.A_CustomerCompany_ = csn.A_CustomerCompany
+// A_CustomerCompanyText
+module.exports.A_CustomerCompanyText = { is_singular: true, __proto__: csn.A_CustomerCompanyText }
+module.exports.A_CustomerCompanyText_ = csn.A_CustomerCompanyText
+// A_CustomerDunning
+module.exports.A_CustomerDunning = { is_singular: true, __proto__: csn.A_CustomerDunning }
+module.exports.A_CustomerDunning_ = csn.A_CustomerDunning
+// A_CustomerSalesArea
+module.exports.A_CustomerSalesArea = { is_singular: true, __proto__: csn.A_CustomerSalesArea }
+module.exports.A_CustomerSalesArea_ = csn.A_CustomerSalesArea
+// A_CustomerSalesAreaTax
+module.exports.A_CustomerSalesAreaTax = { is_singular: true, __proto__: csn.A_CustomerSalesAreaTax }
+module.exports.A_CustomerSalesAreaTax_ = csn.A_CustomerSalesAreaTax
+// A_CustomerSalesAreaText
+module.exports.A_CustomerSalesAreaText = { is_singular: true, __proto__: csn.A_CustomerSalesAreaText }
+module.exports.A_CustomerSalesAreaText_ = csn.A_CustomerSalesAreaText
+// A_CustomerTaxGrouping
+module.exports.A_CustomerTaxGrouping = { is_singular: true, __proto__: csn.A_CustomerTaxGrouping }
+module.exports.A_CustomerTaxGrouping_ = csn.A_CustomerTaxGrouping
+// A_CustomerText
+module.exports.A_CustomerText = { is_singular: true, __proto__: csn.A_CustomerText }
+module.exports.A_CustomerText_ = csn.A_CustomerText
+// A_CustomerUnloadingPoint
+module.exports.A_CustomerUnloadingPoint = { is_singular: true, __proto__: csn.A_CustomerUnloadingPoint }
+module.exports.A_CustomerUnloadingPoint_ = csn.A_CustomerUnloadingPoint
+// A_CustomerWithHoldingTax
+module.exports.A_CustomerWithHoldingTax = { is_singular: true, __proto__: csn.A_CustomerWithHoldingTax }
+module.exports.A_CustomerWithHoldingTax_ = csn.A_CustomerWithHoldingTax
+// A_CustSalesPartnerFunc
+module.exports.A_CustSalesPartnerFunc = { is_singular: true, __proto__: csn.A_CustSalesPartnerFunc }
+module.exports.A_CustSalesPartnerFunc_ = csn.A_CustSalesPartnerFunc
+// A_CustSlsAreaAddrDepdntInfo
+module.exports.A_CustSlsAreaAddrDepdntInfo = { is_singular: true, __proto__: csn.A_CustSlsAreaAddrDepdntInfo }
+module.exports.A_CustSlsAreaAddrDepdntInfo_ = csn.A_CustSlsAreaAddrDepdntInfo
+// A_CustSlsAreaAddrDepdntTaxInfo
+module.exports.A_CustSlsAreaAddrDepdntTaxInfo = { is_singular: true, __proto__: csn.A_CustSlsAreaAddrDepdntTaxInfo }
+module.exports.A_CustSlsAreaAddrDepdntTaxInfo_ = csn.A_CustSlsAreaAddrDepdntTaxInfo
+// A_CustUnldgPtAddrDepdntInfo
+module.exports.A_CustUnldgPtAddrDepdntInfo = { is_singular: true, __proto__: csn.A_CustUnldgPtAddrDepdntInfo }
+module.exports.A_CustUnldgPtAddrDepdntInfo_ = csn.A_CustUnldgPtAddrDepdntInfo
+// A_Supplier
+module.exports.A_Supplier = { is_singular: true, __proto__: csn.A_Supplier }
+module.exports.A_Supplier_ = csn.A_Supplier
+// A_SupplierCompany
+module.exports.A_SupplierCompany = { is_singular: true, __proto__: csn.A_SupplierCompany }
+module.exports.A_SupplierCompany_ = csn.A_SupplierCompany
+// A_SupplierCompanyText
+module.exports.A_SupplierCompanyText = { is_singular: true, __proto__: csn.A_SupplierCompanyText }
+module.exports.A_SupplierCompanyText_ = csn.A_SupplierCompanyText
+// A_SupplierDunning
+module.exports.A_SupplierDunning = { is_singular: true, __proto__: csn.A_SupplierDunning }
+module.exports.A_SupplierDunning_ = csn.A_SupplierDunning
+// A_SupplierPartnerFunc
+module.exports.A_SupplierPartnerFunc = { is_singular: true, __proto__: csn.A_SupplierPartnerFunc }
+module.exports.A_SupplierPartnerFunc_ = csn.A_SupplierPartnerFunc
+// A_SupplierPurchasingOrg
+module.exports.A_SupplierPurchasingOrg = { is_singular: true, __proto__: csn.A_SupplierPurchasingOrg }
+module.exports.A_SupplierPurchasingOrg_ = csn.A_SupplierPurchasingOrg
+// A_SupplierPurchasingOrgText
+module.exports.A_SupplierPurchasingOrgText = { is_singular: true, __proto__: csn.A_SupplierPurchasingOrgText }
+module.exports.A_SupplierPurchasingOrgText_ = csn.A_SupplierPurchasingOrgText
+// A_SupplierText
+module.exports.A_SupplierText = { is_singular: true, __proto__: csn.A_SupplierText }
+module.exports.A_SupplierText_ = csn.A_SupplierText
+// A_SupplierWithHoldingTax
+module.exports.A_SupplierWithHoldingTax = { is_singular: true, __proto__: csn.A_SupplierWithHoldingTax }
+module.exports.A_SupplierWithHoldingTax_ = csn.A_SupplierWithHoldingTax
+// events
+// actions
+// enums
