6f35e3999d8d829ce5a6112ed23d801baf5c6a65
dragolea <dragolea@yahoo.com>
2025-10-14 15:37:32 +0300

    test(catalog-service): fixed by exporting default instead of old common js style

--- test/bookshop/srv/controller/cat-service/catalog-service.ts
+++ test/bookshop/srv/controller/cat-service/catalog-service.ts
@@ -2,7 +2,7 @@ import { CDSDispatcher } from '@dxfrontier/cds-ts-dispatcher';
 
 import BookHandler from './handler/BookHandler';
 
-export = new CDSDispatcher([
+export default new CDSDispatcher([
   // Entities
   BookHandler,
 ]).initialize();
