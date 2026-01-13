d9eeb3107ba59e14ff79fa90e5b42073da0a439f
dragolea <dragolea@yahoo.com>
2026-01-13 11:36:48 +0200

    chore(prettier): prettied files

--- test/bookshop/@cds-models/CatalogService/index.js
+++ test/bookshop/@cds-models/CatalogService/index.js
@@ -1,34 +1,45 @@
 // This is an automatically generated file. Please do not change its contents manually!
-const { createEntityProxy } = require('./../_')
+const { createEntityProxy } = require('./../_');
 // service
-const CatalogService = { name: 'CatalogService' }
-module.exports = CatalogService
-module.exports.CatalogService = CatalogService
+const CatalogService = { name: 'CatalogService' };
+module.exports = CatalogService;
+module.exports.CatalogService = CatalogService;
 // BookEvents
-module.exports.BookEvent = createEntityProxy(['CatalogService', 'BookEvents'], { target: { is_singular: true }, customProps: ["types"] })
-module.exports.BookEvents = createEntityProxy(['CatalogService', 'BookEvents'], { target: { is_singular: false }})
+module.exports.BookEvent = createEntityProxy(['CatalogService', 'BookEvents'], {
+  target: { is_singular: true },
+  customProps: ['types'],
+});
+module.exports.BookEvents = createEntityProxy(['CatalogService', 'BookEvents'], { target: { is_singular: false } });
 // Authors
-module.exports.Author = createEntityProxy(['CatalogService', 'Authors'], { target: { is_singular: true } })
-module.exports.Authors = createEntityProxy(['CatalogService', 'Authors'], { target: { is_singular: false }})
+module.exports.Author = createEntityProxy(['CatalogService', 'Authors'], { target: { is_singular: true } });
+module.exports.Authors = createEntityProxy(['CatalogService', 'Authors'], { target: { is_singular: false } });
 // Books
-module.exports.Book = createEntityProxy(['CatalogService', 'Books'], { target: { is_singular: true } })
-module.exports.Books = createEntityProxy(['CatalogService', 'Books'], { target: { is_singular: false }})
+module.exports.Book = createEntityProxy(['CatalogService', 'Books'], { target: { is_singular: true } });
+module.exports.Books = createEntityProxy(['CatalogService', 'Books'], { target: { is_singular: false } });
 // Currencies
-module.exports.Currency = createEntityProxy(['CatalogService', 'Currencies'], { target: { is_singular: true } })
-module.exports.Currencies = createEntityProxy(['CatalogService', 'Currencies'], { target: { is_singular: false }})
+module.exports.Currency = createEntityProxy(['CatalogService', 'Currencies'], { target: { is_singular: true } });
+module.exports.Currencies = createEntityProxy(['CatalogService', 'Currencies'], { target: { is_singular: false } });
 // Genres
-module.exports.Genre = createEntityProxy(['CatalogService', 'Genres'], { target: { is_singular: true } })
-module.exports.Genres = createEntityProxy(['CatalogService', 'Genres'], { target: { is_singular: false }})
+module.exports.Genre = createEntityProxy(['CatalogService', 'Genres'], { target: { is_singular: true } });
+module.exports.Genres = createEntityProxy(['CatalogService', 'Genres'], { target: { is_singular: false } });
 // Books.texts
-module.exports.Books.text = createEntityProxy(['CatalogService', 'Books.texts'], { target: { is_singular: true } })
-module.exports.Books.texts = createEntityProxy(['CatalogService', 'Books.texts'], { target: { is_singular: false }})
+module.exports.Books.text = createEntityProxy(['CatalogService', 'Books.texts'], { target: { is_singular: true } });
+module.exports.Books.texts = createEntityProxy(['CatalogService', 'Books.texts'], { target: { is_singular: false } });
 // Currencies.texts
-module.exports.Currencies.text = createEntityProxy(['CatalogService', 'Currencies.texts'], { target: { is_singular: true } })
-module.exports.Currencies.texts = createEntityProxy(['CatalogService', 'Currencies.texts'], { target: { is_singular: false }})
+module.exports.Currencies.text = createEntityProxy(['CatalogService', 'Currencies.texts'], {
+  target: { is_singular: true },
+});
+module.exports.Currencies.texts = createEntityProxy(['CatalogService', 'Currencies.texts'], {
+  target: { is_singular: false },
+});
 // Genres.texts
-module.exports.Genres.text = createEntityProxy(['CatalogService', 'Genres.texts'], { target: { is_singular: true } })
-module.exports.Genres.texts = createEntityProxy(['CatalogService', 'Genres.texts'], { target: { is_singular: false }})
+module.exports.Genres.text = createEntityProxy(['CatalogService', 'Genres.texts'], { target: { is_singular: true } });
+module.exports.Genres.texts = createEntityProxy(['CatalogService', 'Genres.texts'], { target: { is_singular: false } });
 // events
 // actions
 // enums
-module.exports.BookEvent.types ??= { BOOK_SIGNING: "BOOK_SIGNING", AUTHOR_TALK: "AUTHOR_TALK", BOOK_LUNCH: "BOOK_LUNCH" }
+module.exports.BookEvent.types ??= {
+  BOOK_SIGNING: 'BOOK_SIGNING',
+  AUTHOR_TALK: 'AUTHOR_TALK',
+  BOOK_LUNCH: 'BOOK_LUNCH',
+};
--- test/bookshop/@cds-models/sap/capire/bookshop/index.js
+++ test/bookshop/@cds-models/sap/capire/bookshop/index.js
@@ -1,39 +1,62 @@
 // This is an automatically generated file. Please do not change its contents manually!
-const { createEntityProxy } = require('./../../../_')
+const { createEntityProxy } = require('./../../../_');
 // Books
-module.exports.Book = createEntityProxy(['sap.capire.bookshop', 'Books'], { target: { is_singular: true } })
-module.exports.Books = createEntityProxy(['sap.capire.bookshop', 'Books'], { target: { is_singular: false }})
+module.exports.Book = createEntityProxy(['sap.capire.bookshop', 'Books'], { target: { is_singular: true } });
+module.exports.Books = createEntityProxy(['sap.capire.bookshop', 'Books'], { target: { is_singular: false } });
 // BookStats
-module.exports.BookStat = createEntityProxy(['sap.capire.bookshop', 'BookStats'], { target: { is_singular: true } })
-module.exports.BookStats = createEntityProxy(['sap.capire.bookshop', 'BookStats'], { target: { is_singular: false }})
+module.exports.BookStat = createEntityProxy(['sap.capire.bookshop', 'BookStats'], { target: { is_singular: true } });
+module.exports.BookStats = createEntityProxy(['sap.capire.bookshop', 'BookStats'], { target: { is_singular: false } });
 // Authors
-module.exports.Author = createEntityProxy(['sap.capire.bookshop', 'Authors'], { target: { is_singular: true } })
-module.exports.Authors = createEntityProxy(['sap.capire.bookshop', 'Authors'], { target: { is_singular: false }})
+module.exports.Author = createEntityProxy(['sap.capire.bookshop', 'Authors'], { target: { is_singular: true } });
+module.exports.Authors = createEntityProxy(['sap.capire.bookshop', 'Authors'], { target: { is_singular: false } });
 // Genres
-module.exports.Genre = createEntityProxy(['sap.capire.bookshop', 'Genres'], { target: { is_singular: true } })
-module.exports.Genres = createEntityProxy(['sap.capire.bookshop', 'Genres'], { target: { is_singular: false }})
+module.exports.Genre = createEntityProxy(['sap.capire.bookshop', 'Genres'], { target: { is_singular: true } });
+module.exports.Genres = createEntityProxy(['sap.capire.bookshop', 'Genres'], { target: { is_singular: false } });
 // Reviews
-module.exports.Review = createEntityProxy(['sap.capire.bookshop', 'Reviews'], { target: { is_singular: true } })
-module.exports.Reviews = createEntityProxy(['sap.capire.bookshop', 'Reviews'], { target: { is_singular: false }})
+module.exports.Review = createEntityProxy(['sap.capire.bookshop', 'Reviews'], { target: { is_singular: true } });
+module.exports.Reviews = createEntityProxy(['sap.capire.bookshop', 'Reviews'], { target: { is_singular: false } });
 // BookEvents
-module.exports.BookEvent = createEntityProxy(['sap.capire.bookshop', 'BookEvents'], { target: { is_singular: true }, customProps: ["types"] })
-module.exports.BookEvents = createEntityProxy(['sap.capire.bookshop', 'BookEvents'], { target: { is_singular: false }})
+module.exports.BookEvent = createEntityProxy(['sap.capire.bookshop', 'BookEvents'], {
+  target: { is_singular: true },
+  customProps: ['types'],
+});
+module.exports.BookEvents = createEntityProxy(['sap.capire.bookshop', 'BookEvents'], {
+  target: { is_singular: false },
+});
 // Users
-module.exports.User = createEntityProxy(['sap.capire.bookshop', 'Users'], { target: { is_singular: true } })
-module.exports.Users = createEntityProxy(['sap.capire.bookshop', 'Users'], { target: { is_singular: false }})
+module.exports.User = createEntityProxy(['sap.capire.bookshop', 'Users'], { target: { is_singular: true } });
+module.exports.Users = createEntityProxy(['sap.capire.bookshop', 'Users'], { target: { is_singular: false } });
 // UserActivityLog
-module.exports.UserActivityLog = createEntityProxy(['sap.capire.bookshop', 'UserActivityLog'], { target: { is_singular: true } })
-module.exports.UserActivityLog_ = createEntityProxy(['sap.capire.bookshop', 'UserActivityLog'], { target: { is_singular: false }})
+module.exports.UserActivityLog = createEntityProxy(['sap.capire.bookshop', 'UserActivityLog'], {
+  target: { is_singular: true },
+});
+module.exports.UserActivityLog_ = createEntityProxy(['sap.capire.bookshop', 'UserActivityLog'], {
+  target: { is_singular: false },
+});
 // Promotions
-module.exports.Promotion = createEntityProxy(['sap.capire.bookshop', 'Promotions'], { target: { is_singular: true } })
-module.exports.Promotions = createEntityProxy(['sap.capire.bookshop', 'Promotions'], { target: { is_singular: false }})
+module.exports.Promotion = createEntityProxy(['sap.capire.bookshop', 'Promotions'], { target: { is_singular: true } });
+module.exports.Promotions = createEntityProxy(['sap.capire.bookshop', 'Promotions'], {
+  target: { is_singular: false },
+});
 // Books.texts
-module.exports.Books.text = createEntityProxy(['sap.capire.bookshop', 'Books.texts'], { target: { is_singular: true } })
-module.exports.Books.texts = createEntityProxy(['sap.capire.bookshop', 'Books.texts'], { target: { is_singular: false }})
+module.exports.Books.text = createEntityProxy(['sap.capire.bookshop', 'Books.texts'], {
+  target: { is_singular: true },
+});
+module.exports.Books.texts = createEntityProxy(['sap.capire.bookshop', 'Books.texts'], {
+  target: { is_singular: false },
+});
 // Genres.texts
-module.exports.Genres.text = createEntityProxy(['sap.capire.bookshop', 'Genres.texts'], { target: { is_singular: true } })
-module.exports.Genres.texts = createEntityProxy(['sap.capire.bookshop', 'Genres.texts'], { target: { is_singular: false }})
+module.exports.Genres.text = createEntityProxy(['sap.capire.bookshop', 'Genres.texts'], {
+  target: { is_singular: true },
+});
+module.exports.Genres.texts = createEntityProxy(['sap.capire.bookshop', 'Genres.texts'], {
+  target: { is_singular: false },
+});
 // events
 // actions
 // enums
-module.exports.BookEvent.types ??= { BOOK_SIGNING: "BOOK_SIGNING", AUTHOR_TALK: "AUTHOR_TALK", BOOK_LUNCH: "BOOK_LUNCH" }
+module.exports.BookEvent.types ??= {
+  BOOK_SIGNING: 'BOOK_SIGNING',
+  AUTHOR_TALK: 'AUTHOR_TALK',
+  BOOK_LUNCH: 'BOOK_LUNCH',
+};
